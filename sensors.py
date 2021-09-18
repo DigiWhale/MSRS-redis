3e0ae564e6c7b7727ed800f5f6cb9c46282970ec
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-18 19:42:29 -0400

    1

--- sensors.py
+++ sensors.py
@@ -32,7 +32,8 @@ while True:
     if msg:
       res = json.loads(str(msg['data']).replace("'", ""))
       if (type(res) == type(dict())):
-        print(res['sensor_value']['altitude'])
+        if (res['sensor_type'] == 6)
+          print(res['sensor_value']['altitude'])
       else:
         print(type(res))
     # try:
