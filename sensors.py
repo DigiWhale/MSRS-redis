5dabe8220619d691c0343fa2caa7e66ae72fa421
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-18 16:37:20 -0400

    1

--- sensors.py
+++ sensors.py
@@ -3,23 +3,31 @@ import time
 import redis
 from redistimeseries.client import Client
 rts = Client(redis.Redis(host="localhost", port=6543, db=0))
+count = 0
 try:
-    rts.create('test', labels={'Time':'Series'})
+    rts.create('heading', labels={'Time':'Series'})
+    rts.create('acceleration', labels={'Time':'Series'})
+    rts.create('angle', labels={'Time':'Series'})
+    rts.create('distance', labels={'Time':'Series'})
+    rts.create('speed', labels={'Time':'Series'})
 except:
     pass
 while True:
-    try:
-        rts.add('test', 1, 1.12)
-        rts.add('test', 2, 1.12)
-    except:
-        pass
-    print(rts.get('test'))
-    time.sleep(1)
-    rts.incrby('test',1)
-    try:
-        rts.range('test', 0, -1)
-        rts.range('test', 0, -1, aggregation_type='avg', bucket_size_msec=10)
-        rts.range('test', 0, -1, aggregation_type='sum', bucket_size_msec=10)
-    except:
-        pass
-    rts.info('test').__dict__
+    count += 1
+    rts.add('heading', time.time(), count * .5)
+    print(rts.get('heading'))
+    # try:
+    #     rts.add('test', 1, 1.12)
+    #     rts.add('test', 2, 1.12)
+    # except:
+    #     pass
+    # print(rts.get('test'))
+    # time.sleep(1)
+    # rts.incrby('test',1)
+    # try:
+    #     rts.range('test', 0, -1)
+    #     rts.range('test', 0, -1, aggregation_type='avg', bucket_size_msec=10)
+    #     rts.range('test', 0, -1, aggregation_type='sum', bucket_size_msec=10)
+    # except:
+    #     pass
+    # rts.info('test').__dict__
