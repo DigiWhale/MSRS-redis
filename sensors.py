0e5dfadfd7b4bc14e974dc4c725c413c3782bc1a
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-18 17:52:03 -0400

    1

--- sensors.py
+++ sensors.py
@@ -29,7 +29,7 @@ while True:
     print(rts.get('heading'), rts.get('acceleration'), rts.get('angle'), rts.get('distance'), rts.get('speed'))
     msg = p.get_message()
     if msg:
-      print(msg)
+      print(msg['data']['sensor_type'])
     # try:
     #     rts.add('test', 1, 1.12)
     #     rts.add('test', 2, 1.12)
