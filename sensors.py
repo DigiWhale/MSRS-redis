6914ed9fb35700202524754137a7aae8b8d4f2ca
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-18 17:56:20 -0400

    1

--- sensors.py
+++ sensors.py
@@ -29,7 +29,7 @@ while True:
     print(rts.get('heading'), rts.get('acceleration'), rts.get('angle'), rts.get('distance'), rts.get('speed'))
     msg = p.get_message()
     if msg:
-      print(str(msg['data']).replace("b'{", '{').replace("\"}'", '"}'))
+      print(json.loads(str(msg['data']).replace("b'{", '{').replace("\"}'", '"}')))
     # try:
     #     rts.add('test', 1, 1.12)
     #     rts.add('test', 2, 1.12)
