42100c2fa357ebfe3e0144f4deb0b8c35b9c0408
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-19 11:50:57 -0400

    1

---
+++ subscribe.py
@@ -0,0 +1,21 @@
+import redis
+from redistimeseries.client import Client
+
+
+# rts = Client(host='localhost', port=6379, db=0)
+r = redis.Redis(host='192.168.1.4', port=6379, db=0, password='Redis2019!')
+p = r.pubsub()
+p.subscribe('msrs_raspberry')
+# rts.create('test', labels={'Time':'Series'})
+# rts.add('test', 1, 1.12)
+# rts.add('test', 2, 1.12)
+# rts.get('test')
+# rts.incrby('test',1)
+# rts.range('test', 0, -1)
+# rts.range('test', 0, -1, aggregation_type='avg', bucket_size_msec=10)
+# rts.range('test', 0, -1, aggregation_type='sum', bucket_size_msec=10)
+# rts.info('test').__dict__
+while True:
+  msg = p.get_message()
+  if msg:
+    print(msg['data'])
