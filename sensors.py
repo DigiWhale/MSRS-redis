d6c1635923fd527b4480ea272cc2243166d58a4f
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-20 16:52:15 -0400

    1

--- sensors.py
+++ sensors.py
@@ -54,7 +54,7 @@ while True:
     msg = p.get_message()
     try:
       if msg:
-        print('{' + str(msg['data'])[1:-1] + '}')
+        # print('{' + str(msg['data'])[1:-1] + '}')
         try:
           res = json.loads('{' + str(msg['data'])[1:-1] + '}')
           # res = ast.literal_eval(str(msg['data'])[2:-1])
