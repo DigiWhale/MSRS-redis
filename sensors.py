dcfea8ef3caba1b99ced8746884c52ae43b9906c
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-20 18:54:40 -0400

    1

--- sensors.py
+++ sensors.py
@@ -57,7 +57,7 @@ while True:
         # print('{' + str(msg['data'])[1:-1] + '}')
         res = json.loads('{' + str(msg['data'])[1:-1] + '}')
         # res = ast.literal_eval(str(msg['data'])[2:-1])
-        if (type(res) == type(dict()) and res['sensor_type']):
+        if (type(res) == type(dict()) and len(res) > 2):
           if (res['sensor_type'] == 6):
             # print(res['sensor_type'], res['sensor_value']['altitude'])
             rts.add('altitude', '*', res['sensor_value']['altitude'], duplicate_policy='last')
