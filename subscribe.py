0127901d2c365f04578c8180c6d89b165265ebd7
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-24 11:17:43 -0400

    1

--- subscribe.py
+++ subscribe.py
@@ -1,6 +1,6 @@
 import redis
 from redistimeseries.client import Client
-import json
+
 
 # rts = Client(host='localhost', port=6379, db=0)
 r = redis.Redis(host='192.168.1.4', port=6379, db=0, password='Redis2019!')
@@ -18,6 +18,4 @@ p.subscribe('msrs_raspberry', 'msrs_sensor')
 while True:
   msg = p.get_message()
   if msg:
-    res = json.loads('{' + str(msg['data'])[1:-1] + '}'.replace("'", '"'))
-    if (type(res) == type(dict())):
-      print(res) 
+    print(msg['data'])
